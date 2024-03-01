41fbe432839385c954963000cb9d309c1f58e931
Iulia Pahopol <iulikamai2001@mail.ru>
2024-03-01 14:45:22 +0200

    catalog added

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,12 @@ $(document).ready(function () {
         ]
 
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('.catalog__tab_active').siblings().removeClass('.catalog__tab_active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
 });
 // $(document).ready(function() {
  

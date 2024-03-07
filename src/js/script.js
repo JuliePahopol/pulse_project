ed618c14e1300676935b04619720c9138deacabf
Iulia Pahopol <iulikamai2001@mail.ru>
2024-03-07 10:23:47 +0200

    footer added

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
+        slidesToShow: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src = "../icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src = "../icons/right.png"></button>',
         responsive: [
@@ -19,9 +20,28 @@ $(document).ready(function () {
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
-          .addClass('.catalog__tab_active').siblings().removeClass('.catalog__tab_active')
-          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+        //в первой строке addClass добавляем active если класс неактивен и наоборот
+        //во сторой closest , мы добавляем класс активности тем элементам у которых их нет и наоборот
+
+        $('.catalog-item__link').each(function(i) {
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+
+        $('.catalog-item__back').each(function(i) {
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+        //this - ссылаемся на каждый элемент который перебираем
 });
 // $(document).ready(function() {
  
